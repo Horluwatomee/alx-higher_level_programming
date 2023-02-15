6c6a4c24eda54762c4295a74f828b837a42b66cf
Horluwatomee <fehinglobal14@gmail.com>
2023-02-15 04:22:44 -0800

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,2 @@
---lists all records of the table second_table of the database hbtn_0c_0
---score and name should be listed in descending order
---  passed as an argument to the mysql command
+-- lista all records in a table except those wil no name value
 SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
