3f6938d909e690ec02882291dba3d9a85a72a81e
Horluwatomee <fehinglobal14@gmail.com>
2023-02-15 04:17:24 -0800

    List by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
--- lists all records(rows) of second_table of the hbtn_0c_0 in MYSQL server
--- display both the score and the name
--- Records should be ordered by score (top first)
---passed as an argument of the mysql command
+-- lists all rows of the table second_table
+-- from the database hbtn_0c_0
+-- results display score and name
+-- ordered by score
 SELECT score, name FROM second_table ORDER BY score DESC;
